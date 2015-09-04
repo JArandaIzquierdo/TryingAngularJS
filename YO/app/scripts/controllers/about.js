e5b24d607f27537fb1dd22ea6f2f628bdcc09239
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-09-04 17:36:35 +0200

    Probando Yeoman

---
+++ YO/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tryingAngularJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tryingAngularJsApp
+ */
+angular.module('tryingAngularJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ YO/app/scripts/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tryingAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tryingAngularJsApp
+ */
+angular.module('tryingAngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'E2E',
+      'Protactor'
+    ];
+  });
